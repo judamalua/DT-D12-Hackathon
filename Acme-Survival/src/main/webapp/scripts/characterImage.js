f83980dbbe88fc67c809a12fb818a761d2bc5366
Daniel Diment <danieldiment.gj@gmail.com>
2018-05-15 18:46:48 +0200

    Fixed bugs

--- Acme-Survival/src/main/webapp/scripts/characterImage.js
+++ Acme-Survival/src/main/webapp/scripts/characterImage.js
@@ -1,7 +1,6 @@
 function generateCharacters() {
-	debugger;
-	var genre = document.getElementById("charaterGenre").innerHtml;
-	var name = document.getElementById("characterName").value;
+	var genre = document.getElementsByClassName("charaterGenre")[0].innerHTML;
+	var name = document.getElementsByClassName("characterName")[0].innerHTML;
 	var avatars;
 	if (genre === "Male") {
 		avatars = new Avatars(Avatars.sprites.male);
@@ -13,5 +12,5 @@ function generateCharacters() {
 		avatars = '<svg><text x="0" y="15" fill="red">Sorry, only two genders are supported</text></svg>';
 	}
 	var svg = avatars.create(name);
-	document.getElementById("characterImage").innerHTML = svg;
+	document.getElementsByByClassName("characterImage")[0].innerHTML = svg;
 }
