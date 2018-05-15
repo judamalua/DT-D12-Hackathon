2d91cbf7fbf12b6063bb1d8fa83f230a224309d1
Daniel Diment <danieldiment.gj@gmail.com>
2018-05-15 18:49:18 +0200

    Fixed more things, I like fixing stuff

--- Acme-Survival/src/main/webapp/scripts/characterImage.js
+++ Acme-Survival/src/main/webapp/scripts/characterImage.js
@@ -1,16 +1,18 @@
 function generateCharacters() {
-	var genre = document.getElementsByClassName("charaterGenre")[0].innerHTML;
-	var name = document.getElementsByClassName("characterName")[0].innerHTML;
-	var avatars;
-	if (genre === "Male") {
-		avatars = new Avatars(Avatars.sprites.male);
+	for ( var int = 0; int < document.getElementsByClassName("charaterGenre").length; int++) {
+		var genre = document.getElementsByClassName("charaterGenre")[int].innerHTML;
+		var name = document.getElementsByClassName("characterName")[int].innerHTML;
+		var avatars;
+		if (genre === "Male") {
+			avatars = new Avatars(Avatars.sprites.male);
+		}
+		if (genre === "Female") {
+			avatars = new Avatars(Avatars.sprites.female);
+		}
+		if (genre !== "Male" && genre !== "Female") {
+			avatars = '<svg><text x="0" y="15" fill="red">Sorry, only two genders are supported</text></svg>';
+		}
+		var svg = avatars.create(name);
+		document.getElementsByByClassName("characterImage")[int].innerHTML = svg;
 	}
-	if (genre === "Female") {
-		avatars = new Avatars(Avatars.sprites.female);
-	}
-	if (genre !== "Male" && genre !== "Female") {
-		avatars = '<svg><text x="0" y="15" fill="red">Sorry, only two genders are supported</text></svg>';
-	}
-	var svg = avatars.create(name);
-	document.getElementsByByClassName("characterImage")[0].innerHTML = svg;
 }
