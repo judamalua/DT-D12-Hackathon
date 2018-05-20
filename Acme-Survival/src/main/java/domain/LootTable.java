b045aecbcbe76cf7279e0fc06340b462c043b811
Alejandro Guerrero Díaz <alexgue404@gmail.com>
2018-05-20 12:32:58 +0200

    LootTable functions implemented

--- Acme-Survival/src/main/java/domain/LootTable.java
+++ Acme-Survival/src/main/java/domain/LootTable.java
@@ -1,7 +1,13 @@
 
 package domain;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
 
 import javax.persistence.Access;
 import javax.persistence.AccessType;
@@ -65,6 +71,53 @@ public class LootTable extends DomainEntity {
 
 	public void setProbabilityItems(final Collection<ProbabilityItem> probabilityItems) {
 		this.probabilityItems = probabilityItems;
+	}
+	
+	
 
+	public List<ItemDesign> getResultItems(Integer luck, Integer capacity){
+		Random randomGenerator = new Random();
+		
+		List<ItemDesign> items = new ArrayList<ItemDesign>();
+		
+		Comparator<ProbabilityItem> comparator = new Comparator<ProbabilityItem>() {
+		    @Override
+		    public int compare(ProbabilityItem left, ProbabilityItem right) {
+		        return (left.getValue() > right.getValue()) ? 1 : -1; // use your logic
+		    }
+		};
+
+		
+		
+		List<ProbabilityItem> probItems = new ArrayList<ProbabilityItem>(this.probabilityItems);
+		
+		Collections.sort(probItems, comparator);
+		
+		for (ProbabilityItem probItem : probItems){
+			
+			if (probItem.getValue()*(1+(luck/100.0)) > randomGenerator.nextDouble()){
+				items.add(probItem.getItemDesign());
+			}
+			if (items.size() >= capacity){
+				break;
+			}
+		}
+		
+		return items;
+	}
+	
+	public List<Event> getResultEvents(Integer luck){
+		Random randomGenerator = new Random();	
+
+		List<Event> events = new ArrayList<Event>();
+		for (ProbabilityEvent probevent : this.probabilityEvents){
+			
+			if (probevent.getValue() > randomGenerator.nextDouble()){
+				events.add(probevent.getEvent());
+			}
+			
+		}
+		
+		return events;
 	}
 }
